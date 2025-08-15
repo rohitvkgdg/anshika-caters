773aec4cdfd03755159804e4563a918cef77d22f
Rohit <rohitvkgdg@gmail.com>
2025-08-15 23:09:59 +0530

    Fixed favicon name

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/favicon.ico", type: "image/x-icon" },
     ],
     apple: [
